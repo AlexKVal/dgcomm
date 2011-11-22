f2e8125f240e8c5b4d1b5023bdf9aeca69762877
Sasha <sashainkiev@gmail.com>
2011-11-22 21:16:16 +0200

    product.description migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111113160210) do
+ActiveRecord::Schema.define(:version => 20111122190646) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   add_index "products", ["name"], :name => "index_products_on_name", :unique => true
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Product do
   before(:each) do
     @product = Factory(:product)
-    @valid_attrs = FactoryGirl.attributes_for(:product)
+    @valid_attrs = Factory.attributes_for(:product)
   end
 
   it "should create a new instance given valid attributes" do
@@ -13,10 +13,6 @@ describe Product do
   describe "name attr" do
     it "should have a name attribute" do
       @product.should respond_to(:name)
-    end
-
-    it "is valid with valid attributes" do
-      @product.should be_valid
     end
 
     it "is not valid w/o a name" do
@@ -34,6 +30,12 @@ describe Product do
       same_name_product = Factory(:product)
       same_name_product.name = @product.name
       same_name_product.should_not be_valid
+    end
+  end
+
+  describe "description attr" do
+    it "should have a description attribute" do
+      @product.should respond_to(:description)
     end
   end
 end
